bd8a26f95ba14fd3f8fe6046b1b894d88318b10f
kost <kost9937@gmail.com>
2023-08-31 13:51:24 +0300

    Refactored interface suitable

--- src/main/java/com/example/sweater/repos/DaoRepos.java
+++ src/main/java/com/example/sweater/repos/DaoRepos.java
@@ -1,9 +1,11 @@
 package com.example.sweater.repos;
 
 import com.example.sweater.domain.DtoJoke;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public interface DaoRepos {
 
 	List<DtoJoke> getJokesByCount(int count);
