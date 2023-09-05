39f2ee71c958c55f1e404c96549b28bb71ebbb20
kost <kost9937@gmail.com>
2023-09-05 12:29:22 +0300

    Add One-to-Many relations

--- src/main/java/com/example/sweater/controller/MainController.java
+++ src/main/java/com/example/sweater/controller/MainController.java
@@ -1,8 +1,10 @@
 package com.example.sweater.controller;
 
 import com.example.sweater.domain.Message;
+import com.example.sweater.domain.User;
 import com.example.sweater.repos.MessageRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -27,9 +29,12 @@ import java.util.Map;
 	}
 
 	@PostMapping("/main")
-	public String add(@RequestParam String text, @RequestParam String tag, Map<String, Object> model) {
+	public String add(
+			@AuthenticationPrincipal User user,
+			@RequestParam String text,
+			@RequestParam String tag, Map<String, Object> model) {
 		if (text != null && !text.isEmpty()){
-			Message message = new Message(text, tag);
+			Message message = new Message(text, tag, user);
 			messageRepo.save(message);
 		}
 		model.put("messages", messageRepo.findAll());
