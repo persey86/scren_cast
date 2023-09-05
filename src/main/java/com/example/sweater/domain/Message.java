3aeedc69fd653826370b22638ed8b61c00a2b3b3
kost <kost9937@gmail.com>
2023-09-05 19:52:23 +0300

    It has been added uploading files on server

--- src/main/java/com/example/sweater/domain/Message.java
+++ src/main/java/com/example/sweater/domain/Message.java
@@ -16,6 +16,8 @@ public class Message {
 	@JoinColumn(name = "user_id")
 	private User author;
 
+	private String filename;
+
 	public Message() {
 	}
 
@@ -61,4 +63,11 @@ public class Message {
 		this.author = author;
 	}
 
+	public String getFilename() {
+		return filename;
+	}
+
+	public void setFilename(String filename) {
+		this.filename = filename;
+	}
 }
